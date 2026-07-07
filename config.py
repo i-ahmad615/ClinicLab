215ba9470cfe24e5cf5eae2f1b1d5427476e4425
i-ahmad615 <ahmadsharjeel615@gmail.com>
2026-07-07 19:25:50 +0500

    Updated config to fix psycopg

--- config.py
+++ config.py
@@ -8,7 +8,9 @@ class Config:
 
     db_url = os.environ.get("DATABASE_URL")
     if db_url and db_url.startswith("postgres://"):
-        db_url = db_url.replace("postgres://", "postgresql://", 1)
+        db_url = db_url.replace("postgres://", "postgresql+psycopg://", 1)
+    elif db_url and db_url.startswith("postgresql://"):
+        db_url = db_url.replace("postgresql://", "postgresql+psycopg://", 1)
 
     SQLALCHEMY_DATABASE_URI = db_url or (
         "sqlite:///" + os.path.join(BASE_DIR, "instance", "clinic.db")
